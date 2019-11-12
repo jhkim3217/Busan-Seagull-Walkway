937663d088491f817f72d89b8db4232b180e4312
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-11-12 13:15:45 +0900

    Initial Commit

---
+++ exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exam
+//
+//  Created by D7702_09 on 2019. 11. 12..
+//  Copyright © 2019년 csd5766. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
